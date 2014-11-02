9d7d252edf10e967994206b665567b2dd097ce5c
JohnnyHu9 <r17d09@gmail.com>
2014-11-02 10:28:19 -0500

    create service interface

--- COMP306_FeedbackService/FeedbackService.cs
+++ COMP306_FeedbackService/FeedbackService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace COMP306_FeedbackService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class FeedbackService : IFeedbackService
     {
         public string GetData(int value)
         {
--- COMP306_FeedbackService/IFeedbackService.cs
+++ COMP306_FeedbackService/IFeedbackService.cs
@@ -9,7 +9,7 @@ namespace COMP306_FeedbackService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IFeedbackService
     {
         [OperationContract]
         string GetData(int value);
