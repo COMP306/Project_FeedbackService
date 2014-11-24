da7ee7459e4e73a71702aa34d4b6ef22d5323788
JohnnyHu9 <r17d09@gmail.com>
2014-11-24 00:22:00 -0500

    update sql scripts

--- IISTestClient/Program.cs
+++ IISTestClient/Program.cs
@@ -12,13 +12,15 @@ namespace IISTestClient
         {
             PermissiveCertificatePolicy.Enact("CN=HTTPS-Server");
 
+            WCFServiceReference.FeedbackServiceClient client = new WCFServiceReference.FeedbackServiceClient("WS2007HttpBinding_IFeedbackService");
             try
             {
-                WCFServiceReference.FeedbackServiceClient client = new WCFServiceReference.FeedbackServiceClient("WS2007HttpBinding_IFeedbackService");
 
-                client.ClientCredentials.Windows.ClientCredential.Domain = "AlexLiu-PC";
-                client.ClientCredentials.Windows.ClientCredential.UserName = "alexliu";
-                client.ClientCredentials.Windows.ClientCredential.Password = "Password";
+                client.ClientCredentials.Windows.ClientCredential.Domain = Environment.MachineName; // "AlexLiu-PC";
+                //client.ClientCredentials.Windows.ClientCredential.UserName = "alexliu";
+                //client.ClientCredentials.Windows.ClientCredential.Password = "Password";
+                client.ClientCredentials.Windows.ClientCredential.UserName = "student";
+                client.ClientCredentials.Windows.ClientCredential.Password = "password";
                 //client.ClientCredentials.Windows.ClientCredential.UserName = "visitor";
                 //client.ClientCredentials.Windows.ClientCredential.Password = "visitor";
 
@@ -34,13 +36,43 @@ namespace IISTestClient
                 //System.Net.ServicePointManager.ServerCertificateValidationCallback
                 //    = ((sender, cert, chain, errors) => cert.Subject.Contains("AlexLiu-PC")); //Servername
 
-                string user = client.GetCurrentUser();
-                Console.WriteLine("You are : {0}", user);
+                // test to get current user
+                string user = "";
+                try
+                {
+                    Console.WriteLine("Loading current user...");
+                    user = client.GetCurrentUser();
+                    Console.WriteLine("You are : {0}\n", user);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: failed to get current user. please add user to group [FeedbackUsers]");
+                    Console.WriteLine(ex.Message);
+                }
 
+
+                // test to call get all course
+                Console.WriteLine("\nLoadin all courses:");
                 var r = client.GetAllCourse();
                 foreach (var c in r)
                 {
                     Console.WriteLine("ID:{0},   Code:{1},   Title:{2}.", c.ID, c.Code, c.Title);
+                }
+
+                // test to call post feedback
+                string feedback = "some text";
+                int course_id = 1;
+                Console.WriteLine(string.Format("\nUser[{0}], adding new feedback:{1} to course id:{2}.", user, feedback, course_id));
+                try
+                {
+                    int result = client.PostFeedbackByCourseID(course_id, feedback, false);
+                    if (result > 0)
+                        Console.WriteLine("New feedback added.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: add feedback failed.  (*please make sure user exists in TStudent and TStudentCourse)");
+                    Console.WriteLine(ex.Message);
                 }
 
                 client.Close();
@@ -49,6 +81,11 @@ namespace IISTestClient
             {
                 Console.WriteLine(ex.Message);
             }
+            finally
+            {
+                client.Close();
+            }
+            Console.Write("\nPress enter to exit.");
             Console.ReadLine();
         }
     }
